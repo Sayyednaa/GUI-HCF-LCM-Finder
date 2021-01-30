1ef8ecf75349add38d521eed2fb0d4f7bf34f6fd
Sayyed Nawab Abdul Ali <70070584+Sayyednaa@users.noreply.github.com>
2021-01-30 14:00:15 +0530

    Delete hcf.py

--- hcf.py
+++
@@ -1,9 +0,0 @@
-# Function to find HCF the Using Euclidian algorithm
-def compute_hcf(x, y):
-   while(y):
-       x, y = y, x % y
-   return x
-# num = int(input('Enter First Number:\n'))
-# ns = int(input('Enter Second Number:\n'))
-# hcf = compute_hcf(num, ns)
-# print("The HCF is", hcf)
